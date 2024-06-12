2cbd00f6fc473fe76fd3a78d22e0be5f26c5a45e
Laxmidevi1 <Yerramasi.Mounika@ibm.com>
2024-06-12 16:00:27 +0530

    Add files via upload

---
+++ TestNG/Activities/TestNG_Examples.java
@@ -0,0 +1,9 @@
+package examples;
+
+import org.testng.annotations.BeforeClass;
+
+public class TestNG_Examples {
+
+    @BeforeClass
+    public void setUpforClass(){System.out.println("Before every test method");}
+}
