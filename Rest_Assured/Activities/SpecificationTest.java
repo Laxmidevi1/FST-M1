b48b762045773fbf3a6cfc24392d9a0341674169
Laxmidevi1 <Yerramasi.Mounika@ibm.com>
2024-07-29 15:54:22 +0530

    Add files via upload

---
+++ Rest_Assured/Activities/SpecificationTest.java
@@ -0,0 +1,11 @@
+package Examples;
+
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+public class SpecificationTest
+{
+    RequestSpecification requestspec;
+    ResponseSpecification reponcespec;
+    int petId;
+}
